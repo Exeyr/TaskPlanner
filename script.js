44fb81906ec24fe15bdec3c81110ac0926736161
Exeyr <peep.podder@gmail.com>
2022-11-23 15:47:16 +0200

    finished the basic script for generating a new task. Added to the task also an eventlistener function that allows you to click and unclick the task to signify completed status. An additional dblclick eventlistener gets rid of the task entirely

--- script.js
+++ script.js
@@ -14,5 +14,20 @@ function submitOneTimeTask() {
     const oneTimeTaskDiv = document.createElement('div');
     oneTimeTaskDiv.classList.add('oneTimeTaskDiv');
     var text = document.getElementById('oneTask_Text');
+        oneTimeTaskDiv.textContent = text.value;
+    
+    var clickCount = 0;
+    oneTimeTaskDiv.addEventListener('click', function(event){
+        clickCount++;
+        if(clickCount % 2 !== 0) {
+            event.target.style.backgroundColor = 'blue';
+        }else{
+            event.target.style.backgroundColor = 'white';
+        }
+    });
+    
+    oneTimeTaskDiv.addEventListener('dblclick', function(event){
+        oneTimeTaskContainer.removeChild(oneTimeTaskDiv);
+    });
     oneTimeTaskContainer.appendChild(oneTimeTaskDiv);
 }
