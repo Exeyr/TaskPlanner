0b5edc72eb34ff3cefd8c69437c8976bd3910a59
Exeyr <peep.podder@gmail.com>
2022-11-28 13:49:04 +0200

    Added the necessary code for the timed tasks in javascript. It works fine, now just need to style it with CSS and the basic functions are done. Next steps are to 1) Add animations and 2) Make it so that you can save a plan

--- script.js
+++ script.js
@@ -31,3 +31,66 @@ function submitOneTimeTask() {
     });
     oneTimeTaskContainer.appendChild(oneTimeTaskDiv);
 }
+
+const timedTaskContainer = document.querySelector('#timedTasks');
+
+function addTimedTask() {
+    
+    if (document.getElementById("timedTask_Text").value == '') {
+        alert("Add a task name first");
+    }else if (document.getElementById("timedTask_Hours").value == ''){
+        alert("Specify the time for this task");
+    }else{
+        submitTimedTask();
+    }
+}
+
+function submitTimedTask() {
+    const timedTaskDiv = document.createElement('div');
+    timedTaskDiv.classList.add('timedTaskDiv');
+    timedTaskDiv.setAttribute('id', 'timedTaskDiv');
+    var text = document.getElementById('timedTask_Text');
+            timedTaskDiv.textContent = text.value;
+
+    const timedTaskSubDiv = document.createElement('div');
+    timedTaskSubDiv.classList.add('timedTaskSubDiv');
+    timedTaskSubDiv.setAttribute('id', 'timedTaskSubDiv');
+
+
+        const timedTaskOutput = document.createElement('output');
+            timedTaskOutput.classList.add('bubble');
+            timedTaskOutput.setAttribute('id', 'bubble');   
+
+        const timedTaskSlider = document.createElement('input');
+            timedTaskSlider.id = 'hours';
+            timedTaskSlider.type = 'range';
+            timedTaskSlider.min = '0';
+            timedTaskSlider.max = document.getElementById("timedTask_Hours").value;
+            timedTaskSlider.step = '1';
+            timedTaskSlider.addEventListener("input", () => {
+                setBubble(timedTaskSlider, bubble);
+              });
+              
+              function setBubble(timedTaskSlider, bubble) {
+                var val = timedTaskSlider.value;
+                var min = timedTaskSlider.min ? timedTaskSlider.min : 0;
+                var max = timedTaskSlider.max ? timedTaskSlider.max : 100;
+                var newVal = Number(((val - min) * 100) / (max - min));
+                timedTaskOutput.textContent = val;  
+                
+                if (val == '0') {
+                    timedTaskDiv.style.backgroundColor = 'blue';
+                }else{
+                    timedTaskDiv.style.backgroundColor = 'white';
+                }
+              
+                // Sorta magic numbers based on size of the native UI thumb
+                bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
+              }
+    
+    timedTaskContainer.appendChild(timedTaskDiv);
+    timedTaskDiv.appendChild(timedTaskSubDiv)
+    timedTaskSubDiv.appendChild(timedTaskSlider);
+    timedTaskSubDiv.appendChild(timedTaskOutput);
+
+}
